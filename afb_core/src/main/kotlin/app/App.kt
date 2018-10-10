b865f8519e4095ac5e05819690cd600bd5a9d3b6
DaisukeKURATA <d.kruata.38@gmail.com>
2018-10-10 18:47:09 +0900

    write source for my own twitter account

--- afb_core/src/main/kotlin/app/App.kt
+++ afb_core/src/main/kotlin/app/App.kt
@@ -1,2 +1,22 @@
 package app
 
+import com.amazonaws.services.lambda.runtime.Context
+import twitter4j.Twitter
+import twitter4j.TwitterFactory
+
+class App {
+    public fun handler(context: Context) {
+        val lambdaLogger = context.logger
+        val followConditionText = "エンジニア"
+
+        // search follower
+        val twitter: Twitter = TwitterFactory.getSingleton()
+        val followersList = twitter.getFollowersList(null, 1)
+
+        //if not follow and contain engineer, follow back
+        followersList
+                .stream()
+                .filter{t ->  !t.isFollowRequestSent || t.description.contains(followConditionText)}
+                .peek{t -> twitter.createFriendship(t.id); lambdaLogger.log("follow " + t.screenName)}
+    }
+}
