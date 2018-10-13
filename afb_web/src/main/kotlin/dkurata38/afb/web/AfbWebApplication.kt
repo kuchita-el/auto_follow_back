8a2af00e6884a4a9545f362f3365ffadf511a645
DaisukeKURATA <d.kruata.38@gmail.com>
2018-10-13 17:07:38 +0900

    create web project with spring-kotlin by gradle

---
+++ afb_web/src/main/kotlin/dkurata38/afb/web/AfbWebApplication.kt
@@ -0,0 +1,11 @@
+package dkurata38.afb.web
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AfbWebApplication
+
+fun main(args: Array<String>) {
+    runApplication<AfbWebApplication>(*args)
+}
