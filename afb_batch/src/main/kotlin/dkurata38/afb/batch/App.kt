1782cf8fd0f00c3ba4f996c09532642723ef1b40
DaisukeKURATA <daisuke.kurata@example.com>
2018-10-16 10:30:49 +0900

    refactoring build.gradle

--- afb_batch/src/main/kotlin/dkurata38/afb/batch/App.kt
+++ afb_batch/src/main/kotlin/dkurata38/afb/batch/App.kt
@@ -1,28 +1,29 @@
 package app
 
-import com.amazonaws.services.lambda.runtime.Context
 import twitter4j.Twitter
 import twitter4j.TwitterFactory
 
 class App {
-    public fun handler(context: Context) {
-        val lambdaLogger = context.logger
+    public fun handler() {
         val followConditionText = "エンジニア"
 
         // search followerId
         val twitter: Twitter = TwitterFactory.getSingleton()
         val followersIDs = twitter.getFollowersIDs(-1)
         val friendsIDs = twitter.getFriendsIDs(-1)
-        lambdaLogger.log("followersCount: ${followersIDs.iDs.size}, friendsCount: ${friendsIDs.iDs.size}")
+        System.out.println("followersCount: ${followersIDs.iDs.size}, friendsCount: ${friendsIDs.iDs.size}")
 
         val oneWayFriendIds = followersIDs.iDs.filterNot { e -> friendsIDs.iDs.contains(e) }.toLongArray()
         val oneWayFriends = twitter.lookupUsers(*oneWayFriendIds)
-        lambdaLogger.log("oneWayFriendsCount: ${oneWayFriends.size}")
+        System.out.println("oneWayFriendsCount: ${oneWayFriends.size}")
 
         //if not send follow request and contains keyword
         oneWayFriends
                 .stream()
                 .filter{f -> !f.isFollowRequestSent && f.description.contains(followConditionText)}
-                .peek{t -> twitter.createFriendship(t.id); lambdaLogger.log("follow " + t.screenName)}
+                .peek{
+					t -> twitter.createFriendship(t.id)
+					System.out.println("follow " + t.screenName)
+				}
     }
 }
