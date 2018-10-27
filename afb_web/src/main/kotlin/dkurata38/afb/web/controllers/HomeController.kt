62029aaf4a31d881f1a0e65b67197cbe4cef440d
DaisukeKURATA <d.kruata.38@gmail.com>
2018-10-27 14:37:41 +0900

    fix get session logic

--- afb_web/src/main/kotlin/dkurata38/afb/web/controllers/HomeController.kt
+++ afb_web/src/main/kotlin/dkurata38/afb/web/controllers/HomeController.kt
@@ -1,34 +1,24 @@
 package dkurata38.afb.web.controllers
 
-import dkurata38.afb.domain.user.User
-import dkurata38.afb.web.security.UserDetails
-import org.springframework.security.core.Authentication
-import org.springframework.social.connect.ConnectionRepository
-import org.springframework.social.twitter.api.Twitter
+import dkurata38.afb.web.usecase.security.UserSession
+//import org.springframework.social.connect.ConnectionRepository
+//import org.springframework.social.twitter.api.Twitter
 import org.springframework.stereotype.Controller
-import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.servlet.ModelAndView
-import org.springframework.web.servlet.View
-import org.thymeleaf.spring4.view.ThymeleafView
 import java.security.Principal
+import javax.servlet.http.HttpSession
 
 @Controller
 @RequestMapping("/")
-class HomeController {
+class HomeController(private val httpSession: HttpSession) {
 
     @GetMapping
-    fun index(modelAndView: ModelAndView, principal: Principal?): ModelAndView {
-        var user: User? = null
-        if (principal is Authentication){
-            val userDetails = principal.principal
-            if (userDetails is UserDetails){
-                user = userDetails.getUser()
-            }
-        }
+    fun index(modelAndView: ModelAndView): ModelAndView {
+        val userSession = httpSession.getAttribute("userSession") as? UserSession?
         modelAndView.viewName = "/home/index"
-        modelAndView.addObject("user", user)
+        modelAndView.addObject("userSession", userSession)
         return modelAndView
     }
 }
