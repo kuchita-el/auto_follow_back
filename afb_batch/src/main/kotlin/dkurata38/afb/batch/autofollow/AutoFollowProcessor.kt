6be1531c0a1fddd592f07be288be0026916a46b4
DaisukeKURATA <d.kruata.38@gmail.com>
2018-10-28 11:29:06 +0900

    executable batch

---
+++ afb_batch/src/main/kotlin/dkurata38/afb/batch/autofollow/AutoFollowProcessor.kt
@@ -0,0 +1,43 @@
+package dkurata38.afb.batch.autofollow
+
+import dkurata38.afb.domain.automateuser.AutomateUser
+import dkurata38.afb.domain.followkeyword.FollowKeywordRepository
+import dkurata38.afb.domain.user.UserRepository
+import org.slf4j.LoggerFactory
+import org.springframework.batch.item.ItemProcessor
+
+class AutoFollowProcessor(private val userRepository: UserRepository,
+                          private val followKeywordRepository: FollowKeywordRepository): ItemProcessor<AutomateUser, AutoFollowResouceDto> {
+    private val log = LoggerFactory.getLogger(AutoFollowProcessor::class.java)
+
+    /**
+     * Process the provided item, returning a potentially modified or new item for continued
+     * processing.  If the returned result is null, it is assumed that processing of the item
+     * should not continue.
+     *
+     * @param item to be processed
+     * @return potentially modified or new item for continued processing, null if processing of the
+     * provided item should not continue.
+     *
+     * @throws Exception thrown if exception occurs during processing.
+     */
+    override fun process(item: AutomateUser?): AutoFollowResouceDto? {
+        if(item == null) {
+            log.info("アイテムがありません")
+            return null
+        }
+        val user = userRepository.findById(item.getUserId()!!)
+        if (!user.isPresent) {
+            log.warn("存在しないユーザです。")
+            return null
+        }
+
+        val followKeyword = followKeywordRepository.findByUserId(item.getUserId()!!)
+        if(followKeyword == null) {
+            log.warn("キーワードが未設定です。")
+            return null
+        }
+
+        return AutoFollowResouceDto(followKeyword, user.get().getToken())
+    }
+}
--- afb_batch/src/main/kotlin/dkurata38/afb/batch/config/BatchConfig.kt
+++ afb_batch/src/main/kotlin/dkurata38/afb/batch/config/BatchConfig.kt
@@ -1,19 +1,49 @@
 package dkurata38.afb.batch.config
 
-import org.springframework.context.annotation.Configuration
+import dkurata38.afb.batch.autofollow.AutoFollowProcessor
+import dkurata38.afb.batch.autofollow.AutoFollowResouceDto
+import dkurata38.afb.batch.autofollow.AutoFollowWriter
+import dkurata38.afb.domain.autofollow.AutoFollowService
+import dkurata38.afb.domain.automateuser.AutomateUser
+import dkurata38.afb.domain.automateuser.AutomateUserRepository
+import dkurata38.afb.domain.followkeyword.FollowKeywordRepository
+import dkurata38.afb.domain.user.UserRepository
+import org.springframework.batch.core.Job
+import org.springframework.batch.core.Step
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory
+import org.springframework.batch.item.data.RepositoryItemReader
 import org.springframework.context.annotation.Bean
-import dkurata38.afb.batch.autofollow.AutoFollowWriter
-import org.springframework.batch.core.Step
-import dkurata38.afb.domain.twitteruser.TwitterUser
-import org.springframework.batch.core.Job
-import dkurata38.afb.domain.autofollow.AutoFollowService
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.domain.Sort
 
 @Configuration
 @EnableBatchProcessing
-class BatchConfig(val jobBuilderFactory: JobBuilderFactory, val stepBuilderFactory: StepBuilderFactory, val autoFollowService: AutoFollowService) {
+class BatchConfig(
+		private val jobBuilderFactory: JobBuilderFactory,
+		private val stepBuilderFactory: StepBuilderFactory,
+		private val autoFollowService: AutoFollowService,
+		private val automateUserRepository: AutomateUserRepository,
+		private val userRepository: UserRepository,
+		private val followKeywordRepository: FollowKeywordRepository) {
+
+	@Bean
+	fun autoFollowReader(): RepositoryItemReader<AutomateUser> {
+		val repositoryItemReader = RepositoryItemReader<AutomateUser>()
+		repositoryItemReader.setRepository(automateUserRepository)
+		repositoryItemReader.setMethodName("findAll")
+
+		val sort = HashMap<String, Sort.Direction>()
+		sort["id"] = Sort.Direction.ASC
+		return repositoryItemReader
+	}
+
+	@Bean
+	fun autoFollowProcessor(): AutoFollowProcessor{
+		return AutoFollowProcessor(userRepository, followKeywordRepository)
+	}
+
 	@Bean
 	fun autoFollowWriter(): AutoFollowWriter {
 		return AutoFollowWriter(autoFollowService)
@@ -21,15 +51,17 @@ class BatchConfig(val jobBuilderFactory: JobBuilderFactory, val stepBuilderFacto
 	
 	@Bean
 	fun step(): Step {
-		return stepBuilderFactory.get("step")
-				.chunk<TwitterUser, TwitterUser>(10)
+		return stepBuilderFactory.get("step1")
+				.chunk<AutomateUser, AutoFollowResouceDto>(10)
+				.reader(autoFollowReader())
+				.processor(autoFollowProcessor())
 				.writer(autoFollowWriter())
 				.build()
 	}
 	
 	@Bean
 	fun job(): Job {
-		return jobBuilderFactory.get("job")
+		return jobBuilderFactory.get("job1")
 				.start(step())
 				.build()
 	}
